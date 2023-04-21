1866e5dd7b8db05d0fb87b16b32d041d81e4197f
miguelteixeiraa <miguelteixeiraa@users.noreply.github.com>
2023-04-21 01:44:14 +0000

    chore: release v4.0.0

--- include/ada/ada_version.h
+++ include/ada/ada_version.h
@@ -5,12 +5,12 @@
 #ifndef ADA_ADA_VERSION_H
 #define ADA_ADA_VERSION_H
 
-#define ADA_VERSION "3.0.0"
+#define ADA_VERSION "4.0.0"
 
 namespace ada {
 
 enum {
-  ADA_VERSION_MAJOR = 3,
+  ADA_VERSION_MAJOR = 4,
   ADA_VERSION_MINOR = 0,
   ADA_VERSION_REVISION = 0,
 };
