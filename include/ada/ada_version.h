fbcb77ef23afafabea6be287fefa6f1f98ebe756
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2023-04-22 19:14:05 -0300

    fix unresolved conflict

--- include/ada/ada_version.h
+++ include/ada/ada_version.h
@@ -10,15 +10,9 @@
 namespace ada {
 
 enum {
-<<<<<<< HEAD
   ADA_VERSION_MAJOR = 2,
   ADA_VERSION_MINOR = 2,
   ADA_VERSION_REVISION = 0,
-=======
-  ADA_VERSION_MAJOR = 4,
-  ADA_VERSION_MINOR = 5,
-  ADA_VERSION_REVISION = 3,
->>>>>>> f16c30250833fd5446c7279db819daadf740b0a8
 };
 
 }  // namespace ada
