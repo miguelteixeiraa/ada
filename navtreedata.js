0bc1ac97a155342811e5746b62a7a9b9968865b6
miguelteixeiraa <miguelteixeiraa@users.noreply.github.com>
2023-04-22 17:50:10 +0000

    deploy: 30fe708944dc74867e889ed5a21e546cce5acd35

--- navtreedata.js
+++ navtreedata.js
@@ -25,17 +25,7 @@
 var NAVTREE =
 [
   [ "Ada", "index.html", [
-    [ "Requirements", "index.html#autotoc_md1", null ],
-    [ "Usage", "index.html#autotoc_md2", [
-      [ "Parsing & Validation", "index.html#autotoc_md3", null ],
-      [ "Examples", "index.html#autotoc_md4", null ],
-      [ "CMake dependency", "index.html#autotoc_md5", null ]
-    ] ],
-    [ "Contributing", "index.html#autotoc_md6", [
-      [ "Building", "index.html#autotoc_md7", null ],
-      [ "Amalgamation", "index.html#autotoc_md8", null ],
-      [ "License", "index.html#autotoc_md9", null ]
-    ] ],
+    [ "Ada - Testing", "index.html", "index" ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
